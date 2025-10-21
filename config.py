b270b97fc87b78d386a176b38c808ee6a1c96e06
GuGu-99 <jiyeop9600@gmail.com>
2025-10-21 21:01:13 +0900

    Update config.py

--- config.py
+++ config.py
@@ -15,11 +15,11 @@ MONTHLY_BEST_THRESHOLD = 200
 
 # 채널 점수 체계
 CHANNEL_POINTS = {
-    1423170386811682908: {"name": "일일-그림보고", "points": 6, "daily_max": 6, "image_only": True},
-    1423172691724079145: {"name": "자유채팅판", "points": 1, "daily_max": 4, "image_only": False},
-    1423359059566006272: {"name": "정보-공모전", "points": 1, "daily_max": 1, "image_only": False},
-    1423170949477568623: {"name": "정보-그림꿀팁", "points": 1, "daily_max": 1, "image_only": False},
-    1423242322665148531: {"name": "고민상담", "points": 1, "daily_max": 1, "image_only": False},
-    1423359791287242782: {"name": "출퇴근기록", "points": 4, "daily_max": 4, "image_only": False},
-    1423171509752434790: {"name": "다-그렸어요", "points": 5, "daily_max": 5, "image_only": True},
+    1430160521524547678: {"name": "일일-그림보고", "points": 6, "daily_max": 6, "image_only": True},
+    1430160521969139935: {"name": "자유채팅판", "points": 1, "daily_max": 4, "image_only": False},
+    1430160521969139942: {"name": "정보-공모전", "points": 1, "daily_max": 1, "image_only": False},
+    1430160522480717925: {"name": "정보-그림꿀팁", "points": 1, "daily_max": 1, "image_only": False},
+    1430160521969139936: {"name": "고민상담", "points": 1, "daily_max": 1, "image_only": False},
+    1430160521524547677: {"name": "출퇴근기록", "points": 4, "daily_max": 4, "image_only": False},
+    1430160521524547681: {"name": "다-그렸어요", "points": 5, "daily_max": 5, "image_only": True},
 }
